9ee92b89e2b5196d17b390422345a5d0940c6496
Avraneel Pal <avraneelpal22@gmail.com>
2026-06-14 22:11:08 +0530

    set stack as state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import Heading from "./Heading";
 import Scoreboard from "./Scoreboard";
 import CardGrid from "./CardGrid";
 
 export default function App() {
+  const [stack, setStack] = useState([]);
+
   return (
     <>
       <Heading />
