715029727f69d4499d21b36274210f755734064b
Avraneel Pal <avraneelpal22@gmail.com>
2026-06-14 21:59:37 +0530

    commit initial components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+import Scoreboard from "./Scoreboard";
+import CardGrid from "./CardGrid";
+
+export default function App() {
+  return (
+    <>
+      <Heading />
+      <Scoreboard />
+      <CardGrid />:
+    </>
+  );
+}
